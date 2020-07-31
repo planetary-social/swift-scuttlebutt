99cf93f0b045fddb700feeb9a8049c3a33a863f9
月光虹 <iris@random.town>
2020-07-31 18:04:58 +0200

    Refactor integration test expectations according to Bot Activity Phases

--- Tests/ScuttlebuttTests/BotIntegrationTests.swift
+++ Tests/ScuttlebuttTests/BotIntegrationTests.swift
@@ -4,42 +4,44 @@ import XCTest
 
 final class BotIntegrationTests: XCTestCase {
 
+    private func redundantExpectation(description: String) -> XCTestExpectation {
+        let e = expectation(description: description)
+        e.assertForOverFulfill = false
+        return e
+    }
+    
     func testCompleteSetup() {
         let refreshRepeatCount = Int.random(in: 5...10)
         let testDeadline: TimeInterval = 5
         let cancelTime: DispatchTime = .now() + 4
 
-        let ready = expectation(description: "expected ready")
+        let statusExpectations: [ScuttleBot.Phase.Status: XCTestExpectation] = [
+            .ready: expectation(description: "expected bot to be ready"),
+            .willDiscoverPeers: redundantExpectation(description: "expected peer discovery to begin"),
+            .willRefresh: redundantExpectation(description: "expected to see refresh"),
+            .cancelled: expectation(description: "expected cancellation"),
+        ]
 
-        var willDiscoverPeers = expectation(description: "expected peer discovery to begin")
-        willDiscoverPeers.assertForOverFulfill = false
-
-        var peerDiscoveryComplete = expectation(description: "expected peer discovery to complete")
-        peerDiscoveryComplete.assertForOverFulfill = false
-
-        var willRefresh = expectation(description: "expected to see refresh")
-        willRefresh.assertForOverFulfill = false
-
-        var refreshComplete = expectation(description: "expected refresh to complete")
-        refreshComplete.assertForOverFulfill = false
-
-        let cancelled = expectation(description: "expected cancellation")
+        let reportExpectations: [ScuttleBot.Phase.Report: XCTestExpectation] = [
+            .peerDiscoveryComplete: redundantExpectation(description: "expected peer discovery to complete"),
+            .refreshComplete: redundantExpectation(description: "expected peer discovery to complete"),
+        ]
 
         var noFail = expectation(description: "unexpected failure")
         noFail.isInverted = true
 
         let bot = ScuttleBot {
-            ready.fulfill()
+            statusExpectations[.ready]!.fulfill()
         } willDiscoverPeers: {
-            willDiscoverPeers.fulfill()
+            statusExpectations[.willDiscoverPeers]!.fulfill()
         } peerDiscoveryComplete: { report in
-            peerDiscoveryComplete.fulfill()
+            reportExpectations[.peerDiscoveryComplete]!.fulfill()
         } willRefresh: {
-            willRefresh.fulfill()
+            statusExpectations[.willRefresh]!.fulfill()
         } refreshComplete: { report in
-            refreshComplete.fulfill()
+            reportExpectations[.refreshComplete]!.fulfill()
         } cancelled: {
-            cancelled.fulfill()
+            statusExpectations[.cancelled]!.fulfill()
         } failed: { error in
             noFail.fulfill()
         }
