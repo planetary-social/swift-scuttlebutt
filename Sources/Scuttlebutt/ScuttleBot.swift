495440366bfef413ffdd8c9cc3d45bf4722576fd
月光虹 <iris@random.town>
2020-07-31 16:50:09 +0200

    Stub Bot's conformance to Combine's Cancellable protocol

--- Sources/Scuttlebutt/ScuttleBot.swift
+++ Sources/Scuttlebutt/ScuttleBot.swift
@@ -1,9 +1,15 @@
+import Combine
+
 /// ...
-public class ScuttleBot {
+public class ScuttleBot: Cancellable {
 
     /// ...
     public init() {
         // TODO: Implement so that GoBot cane wrapped or extended without disrupting the API too much.
+    }
+
+    public func cancel() {
+        // TODO
     }
 
 }
