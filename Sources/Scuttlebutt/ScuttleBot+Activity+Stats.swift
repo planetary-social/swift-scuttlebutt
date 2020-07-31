bd3092a72bcec0dc5d6c83dfe4cd6108b12a8b54
月光虹 <iris@random.town>
2020-08-01 00:49:40 +0200

    Stub some of the core metrics our Bot should be aware of

--- Sources/Scuttlebutt/ScuttleBot+Activity+Stats.swift
+++ Sources/Scuttlebutt/ScuttleBot+Activity+Stats.swift
@@ -1,9 +1,96 @@
+import Foundation
+
+// FIXME: This are stubs, parts should be probably moved to more abstract places and reused in other packages.
+// Specifically: ProcessingPowerConsumption, DataTansfer...
+
 /// ...
 @available(OSX 10.15, *)
 extension ScuttleBot.Activity {
 
     /// ...
     public struct Stats {
+
+        /// ...
+        public typealias Count = UInt
+
+        /// ...
+        public struct EnergyConsumption {
+
+            /// ...
+            public var interval: TimeInterval
+
+            /// ...
+            public var tasks: Count
+
+        }
+
+        /// ...
+        public struct EnergyBudget {
+            
+            /// ...
+            public let used, wasted: EnergyConsumption
+
+        }
+
+        /// ...
+        public struct PeerOutreach {
+
+            /// ...
+            public var seen, ignored, suggested, adapted: Count
+
+        }
+
+        /// ...
+        public struct DataTransferConsumption {
+
+            /// ...
+            public let attempted, sent, received, corrupted: Count
+
+            /// ...
+            public var total: (read: Count, lost: Count) {
+                (read: sent + received,
+                 lost: attempted - sent + (received - corrupted))
+            }
+
+        }
+
+        /// ...
+        public struct DataTransfer {
+
+            /// ...
+            public let bytes, packets: DataTransferConsumption
+
+        }
+        
+        /// ...
+        public var scheduling: Scheduling
+        
+        /// ...
+        public struct Scheduling {
+
+            /// ...
+            public var energyBudget: EnergyBudget
+
+            // TODO: Calculate scheduling success rate....
+            
+        }
+
+        /// ...
+        public var networking: Networking
+
+        /// ...
+        public struct Networking {
+
+            /// ...
+            public var energyBudget: EnergyBudget
+
+            /// ...
+            public var peerOutreach: PeerOutreach
+
+            /// ...
+            public var dataTransfer: DataTransfer
+
+        }
 
     }
 
