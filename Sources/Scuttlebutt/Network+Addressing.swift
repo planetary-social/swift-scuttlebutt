bbe9de7c2fd382dbfe9d4c645d514b06d73ad448
月光虹 <iris@random.town>
2020-08-02 22:29:51 +0200

    Implement retrieval of all Network Addresses available on the device

--- Sources/Scuttlebutt/Network+Addressing.swift
+++ Sources/Scuttlebutt/Network+Addressing.swift
@@ -1,9 +1,50 @@
+import Darwin
+
 /// ...
 public extension Scuttlebutt.Network {
     
     /// ...
     typealias Address = String
     
+    /// ...
+    static var availableAddresses: [Address] {
+        // XXX: ...
+        var ifaddr: UnsafeMutablePointer<ifaddrs>?
+        
+        guard getifaddrs(&ifaddr) == 0 else {
+            // FIXME: Log error!
+            return []
+        }
+        
+        guard let addr = ifaddr else {
+            // FIXME: Log error!
+            return []
+        }
+        
+        defer {
+            freeifaddrs(ifaddr)
+        }
+
+        return
+            sequence(first: addr.pointee, next: { $0.ifa_next?.pointee })
+            .filter { [UInt8(AF_INET), UInt8(AF_INET6)].contains($0.ifa_addr.pointee.sa_family) }
+            .compactMap { (Scuttlebutt.Network(rawValue: String(cString: $0.ifa_name)), $0) }
+            .compactMap { (network, interface) in
+                var hostNameBuffer = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+
+                getnameinfo(interface.ifa_addr,
+                            socklen_t(interface.ifa_addr.pointee.sa_len),
+                            &hostNameBuffer,
+                            socklen_t(hostNameBuffer.count),
+                            nil,
+                            socklen_t(0),
+                            NI_NUMERICHOST)
+
+                // XXX: Does the buffer need a release, or does the GC takes over?
+                return String(cString: hostNameBuffer)
+            }
+    }
+
     /// ...
     typealias Host = String
 
